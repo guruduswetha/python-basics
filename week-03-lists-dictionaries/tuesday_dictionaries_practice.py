de6778d666f9a322e90c74f5344f144e4b11cc24
Swetha Gurudu <129673960+guruduswetha@users.noreply.github.com>
2026-05-26 17:44:13 -0500

    Add dictionary practice questions 1 to 6

---
+++ week-03-lists-dictionaries/tuesday_dictionaries_practice.py
@@ -0,0 +1,22 @@
+Student_Details = {
+    "Name": "Clarance", 
+    "Age": "28", 
+    "Course": "MSIT", 
+    "City": "Spain"
+    }
+print(Student_Details["Name"])
+print(Student_Details["Age"])
+print(Student_Details["Course"])
+print(Student_Details["City"])
+Student_Details["Certification"] = "PCEP"
+print(Student_Details)
+
+Student_Details.update({"Course": "PCAP Python"})
+print(Student_Details)
+Student_Details.pop("City")
+print(Student_Details)
+
+if "Age" in Student_Details:
+    print("Age is available")
+else:
+    print("Age is not available") 
